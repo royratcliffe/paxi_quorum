c465b0d598565cce9c8b97cb4cb24d446db85ecc
Roy Ratcliffe <royratcliffe@me.com>
2021-03-21 16:22:56 +0000

    Prolog pack

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(paxi_quorum).
+version('0.1.0').
+title('Paxi Quorum').
+author('Roy Ratcliffe', 'royratcliffe@me.com').
+packager('Roy Ratcliffe', 'royratcliffe@me.com').
+maintainer('Roy Ratcliffe', 'royratcliffe@me.com').
+home('https://github.com/royratcliffe/paxi_quorum').
+download('https://github.com/royratcliffe/paxi_quorum/releases/*.zip').
